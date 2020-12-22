82eaee40038a9010d3b6560ff4ccc0705396dc0a
amil13 <40761932+amil13@users.noreply.github.com>
2020-12-22 18:24:10 +0100

    Update JSON_schema.js

--- JSON_schema.js
+++ JSON_schema.js
@@ -1,24 +1,24 @@
   
 {
-    "fields": {                                     #Collection for fields
+    "fields": {                                     //Collection for fields
            "field_id": "unique_field_Id",
            "Name": "La Stalia, Montjuic",
            "PhotoURL": "URL",
-           "Description": {                          #More description can be added such as no. of pitches, size etc.
+           "Description": {                          //More description can be added such as no. of pitches, size etc.
                      "About":"Located in BCN, etc", 
                        "PvP":"8V8",
                       "Type": "Outdoors"
                           },
-            "Location":[{"Address":"add"}, {"coordinates":[54 N, 32 W]}],  #GeoPoint datatype in Firebase
+            "Location":[{"Address":"add"}, {"coordinates":[54 N, 32 W]}],  //GeoPoint datatype in Firebase
             "Slots_available" : [ 
-                    { "slot_id":"unique_slot_id","booked": false,"date&time":"date&time1" }, #Array of timestamps(firebase datatype) for slots available
-                    { "slot_id":"unique_slot_id","booked": false, "date&time":"date&time2"}  #reference to pitch can also be added here in case more than one pitch is available
+                    { "slot_id":"unique_slot_id","booked": false,"date&time":"date&time1" }, //Array of timestamps(firebase datatype) for slots available
+                    { "slot_id":"unique_slot_id","booked": false, "date&time":"date&time2"}  //reference to pitch can also be added here in case more than one pitch is available
                                 ]
             }, 
     
     
-    "players": {                                    #Collection for players
-            "player_id": "unique_player_id",        # other details like games_booked, Fouls, Red_flags, Organizer's_comments can be added
+    "players": {                                    //Collection for players
+            "player_id": "unique_player_id",        //Other details like games_booked, Fouls, Red_flags, Organizer's_comments can be added
             "name": "Eric",
             "sex" : "Male",
             "contact": {
@@ -27,24 +27,24 @@
                        },
             "games_played": 15,
             "last_game_played": "date",
-            "list_of_games_played":[]                 #List of unique game ids
+            "list_of_games_played":[]                //List of unique game ids
         },
     
  
-    "games":{                                         #Collection for games
+    "games":{                                         //Collection for games
         "game_id":"Unique_game_id",
         "date&time":"timestamp",                      
-        "slot_id": "slot_id",                         #from fields
+        "slot_id": "slot_id",                         //from fields
         "organizer": "organizer_id",
         "field": "unique_field_id",  
         "registered_players": ["unique_player_id1","unique_player_id1",...],
         "chatroom": "cid",
-        "review": "rating_id"                           #referencing ratings
+        "review": "rating_id"                           //referencing ratings
     
     },
 
 
-    "ratings":{                                     #Collection for ratings
+    "ratings":{                                     //Collection for ratings
         "rating_id": "unique_rating_id",
         "rater": "player_id",
         "gameid":"game_id",
@@ -58,7 +58,7 @@
         {
             "cid":100,
             "gamechat": false,
-            "ids":[],         #PlayerIds
+            "ids":[/*PlayerIds*/],        
             "messages":[
               {"from":"playerid1", "to":"playerid2", "text":"Hey Dude! You coming to play?"},
               {"from":"playerid2", "to":"playerid1", "text":"Sure man"}
